74b7cc61ec0f2d03b8417fd8ed5f87527d8da2f6
xluki17 <lukasz.z.wawszczak@gmail.com>
2021-05-03 11:09:06 +0200

    wyświetlanie ilości porównań

--- src/main/java/luki17/BubbleSort.java
+++ src/main/java/luki17/BubbleSort.java
@@ -1,68 +1,45 @@
 package luki17;
 
-import java.util.InputMismatchException;
-import java.util.Random;
-import java.util.Scanner;
-
 public class BubbleSort {
 
-    public void run() {
-        int numbersArray = enterNumber("Podaj ilość liczb w tablicy");
-        int[] array = drawNumbers(numbersArray);
+    public void run(int[] array) {
+        System.out.print("Tablica do sortowania:");
         showArray(array);
         System.out.println();
         sort(array);
+        System.out.print("poortowana tablica: ");
         showArray(array);
+        System.out.println();
+        System.out.println();
     }
 
     public static void sort(int array[]) {
-        int numberComparison = 0;
-        boolean hasChange;
-        do {
-            hasChange = false;
-            for (int i = 0; i <= array.length - 2; i++) {
-                if (array[i] > array[i + 1]) {
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                    hasChange = true;
+        if (array == null) {
+            System.out.println("tablica nie może być pusta");
+        } else {
+            boolean hasChange;
+            int numberComparison = 0;
+            int numberElements = array.length;
+            do {
+                hasChange = false;
+                for (int i = 0; i < numberElements - 1; i++) {
                     numberComparison++;
+                    if (array[i] > array[i + 1]) {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        hasChange = true;
+                    }
                 }
-            }
-        } while (hasChange);
-        System.out.println(numberComparison);
-        //showArray(array);
+                numberElements--;
+            } while (numberElements > 1 && hasChange);
+            System.out.println("Ilość porównań to: " + numberComparison);
+        }
     }
 
     private void showArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ", ");
         }
-    }
-
-    private int[] drawNumbers(int nmberArray) {
-        Random random = new Random();
-        int[] array = new int[nmberArray];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100);
-        }
-        return array;
-    }
-
-    private int enterNumber(String text) {
-        Scanner scanner = new Scanner(System.in);
-        int number = 0;
-        boolean isNumber = false;
-        while (!isNumber) {
-            try {
-                System.out.println(text);
-                number = scanner.nextInt();
-                isNumber = true;
-            } catch (InputMismatchException e) {
-                System.out.println("błędne dane! ");
-                scanner.nextLine();
-            }
-        }
-        return number;
     }
 }
--- src/main/java/luki17/Main.java
+++ src/main/java/luki17/Main.java
@@ -4,6 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
         BubbleSort bubble_sort = new BubbleSort();
-        bubble_sort.run();
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {5, 4, 3, 2, 1};
+        int[] array3 = fillAscendingArray();
+        int[] array4 = fillDecendingArray();
+        int[] array5 = {5, 2, 3, 1, 4};
+
+        bubble_sort.run(array1);
+        bubble_sort.run(array2);
+        bubble_sort.run(array3);
+        bubble_sort.run(array4);
+        bubble_sort.run(array5);
+    }
+
+    private static int[] fillAscendingArray() {
+        int[] array = new int[99];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    private static int[] fillDecendingArray() {
+        int[] array = new int[99];
+        int number = array.length;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = number--;
+        }
+        return array;
     }
 }
